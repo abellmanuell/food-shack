818ea12090f22f48c2d1f75d85d6ab0713e15272
Abel Emmanuel <85368219+abellmanuell@users.noreply.github.com>
2023-06-09 00:58:44 +0100

    Update BurgerView.jsx

--- src/component/BurgerView.jsx
+++ src/component/BurgerView.jsx
@@ -38,15 +38,15 @@ export default function BurgerView() {
         throw err;
       });
   }, [products]);
-
+/*
   useEffect(() => {
     axios.get("http://localhost:4000/api/user/1").then((res) => {
       if (res.status >= 200 && res.status <= 299) {
-        //setCustomer(res.data);
+        setCustomer(res.data);
       }
     });
   }, []);
-
+*/
   if (isLoading) {
     return <LoadingSpinner />;
   }
